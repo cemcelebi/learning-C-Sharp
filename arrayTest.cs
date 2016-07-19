1cd92f5b1c6f57771010ad35609cd9c27e7cc45e
Cem Çelebi <cem.celebi.paul@gmail.com>
2016-07-19 16:32:50 +0300

    initial push
    learning c#'s syntax..

---
+++ arrayTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataStructuresTest
+{
+    class arrayTest
+    {
+        static int randomWithRange(int range) {
+            Random r = new Random();
+            int rInt = r.Next(0, range); //for ints
+            return rInt;
+        }
+
+        static int []randomArrayCreator(int range, int size) {
+
+            int [] myArray = new int[size];
+            for (int i = 0; i < size; i++) {
+                myArray[i] = randomWithRange(range);
+            }
+            return myArray;
+        }
+
+        static void arrayPrinter(int [] myArray) {
+            for (int i = 0; i < myArray.Length; i++) {
+                System.Console.WriteLine(myArray[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello from arrayTest");
+            /*template: arrayName = new arrayType[allocationSize];*/
+            int range = 121;
+            int size = 12;
+            int[] myArray = randomArrayCreator(range, size);
+            Console.WriteLine("first element of array is " + myArray[1]);
+            arrayPrinter(myArray);
+        }
+    }
+}
---
+++ helloWorldandFunctionTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataStructuresTest
+{
+    class helloWorldandFunctionTest
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello world");
+            Console.WriteLine("hey hoo "+(4+3));
+            int a = 5;
+            double f = 3.14;
+            while (a > 0)
+            {
+                Console.WriteLine("pi is not " + f);
+                int scopeTest=319;
+                a--;
+            }
+            try {
+                //Console.WriteLine("scopeTest?" + scopeTest);
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine("\n variable created in loop can't be reached from the outside!!!");
+                Console.WriteLine(e.GetBaseException());
+            }
+            int bit1 = 0;
+            int bit2 = 1;
+
+            Console.WriteLine("\n oneBitOrGate of "+bit1+" and "+bit2+" is: "+ oneBitOrGate(bit1,bit2)+"\n");
+
+        }
+        static int oneBitOrGate(int bit1, int bit2) {
+            int returnVal = 0;
+            if (bit1 == 0 && bit2 == 0) {
+                return 0;
+            }
+            if (bit1 == 0 && bit2 == 1)
+            {
+                returnVal=1;
+            }
+            if (bit1 == 1 && bit2 == 0)
+            {
+                returnVal=1;
+            }
+            if (bit1 == 1 && bit2 == 1)
+            {
+                returnVal=1;
+            }
+            return returnVal;
+
+        }
+    }
+
+}
